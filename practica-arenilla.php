1be40266c73e0f840b6af11e1c17a5cb8fde0799
Ana Arenilla <anaarenilla@hotmail.com>
2022-04-22 20:36:01 +0200

    tarea dawdistancia22 actualizada

--- practica-arenilla.php
+++ practica-arenilla.php
@@ -13,4 +13,20 @@ function comprar($precio1,$precio2){
 $total=$precio1+$precio2;
 return $total;
 }
+
+/**
+*@author Ana Arenilla Villalba
+*@version 7.4
+*@param numero1,un número entero.
+*@param numero2,un número entero.
+*@return total , la función devuelve el precio total de la suma de los dos números.
+*{@internal esta función recibre dos números y devuelve el total,
+mensaje para desarrolladores}
+
+*/
+
+function sumar($numero1,$numero2){
+$total=$numero1+$numero2;
+return $total;
+}
 ?>
